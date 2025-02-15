805d90f333709125c27257255a7979ead0a912ec
Inès Bichon <ines.bichon@polytechnique.edu>
2025-02-15 19:50:51 +0100

    continued

--- demandeur/main.py
+++ demandeur/main.py
@@ -5,7 +5,7 @@ api_key = "29NL9Foa83Niu5QLidPK4JMMEwjB7OCx"
 model = "mistral-large-latest"
 
 categories = ("Base", "Electricity", "Food", "Transport", "Infrastructure", "Other")
-args_cat = ({"n_persons": "integer"}, {"is_inside": "boolean", "n_hours": "integer"}, {"menu": "string"}, {"mode": "string", "distance": "float"}, {}, {})
+argsCat = ({"n_persons": "integer"}, {"is_inside": "boolean", "n_hours": "integer"}, {"menu": "string"}, {"mode": "string", "distance": "float"}, {}, {})
 
 descr = {"n_persons": ("Set the number of guests attending the party", "Number of guests attending the party"),
 		 "is_inside": ("Set whether the party is taking place indoors or outdoors", "A boolean that is true if the party is indoors, false if the party is outdoors"),
@@ -15,14 +15,14 @@ descr = {"n_persons": ("Set the number of guests attending the party", "Number o
 		 "distance": ("Set the distance the guests will have to travel", "A floating-points value giving the distance the guests will have to travel, in kilometers"),
 		 }
 
-argsTotal = {key: None for dico in args_cat for key in dico}
+argsTotal = {key: None for dico in argsCat for key in dico}
 dicoEmissions = [None] * len(categories)
 
 tools = []
 
 def build_tools():
 	for i in range(len(categories)):
-		for arg in args_cat[i]:
+		for arg in argsCat[i]:
 			d = {
 					"type": "function",
 					"function": {
@@ -32,7 +32,7 @@ def build_tools():
 							"type": "object",
 							"properties": {
 								arg: {
-									"type": args_cat[i][arg],
+									"type": argsCat[i][arg],
 									"description": descr[arg][1] + ".If it is not explicitly given, ask the user; never try to make up a value.",
 								},
 							},
@@ -88,37 +88,39 @@ def mainloop():
 	return
 	
 
-def set_elec_emissions(is_inside, n_hours):
+def set_elec_emissions(is_inside, n_hours, **kw):
 	if (is_inside):
 		return n_hours*3
 	else:
 		return 0
 
-def set_food_emissions(menu, nbPers):
+def set_food_emissions(menu, nbPers, **kw):
 	# Fonction très simple pour le moment
 	# A long terme, il faut convertir le menu en CO2 à partir d'Agribalise et de Reasoning AI puis faire le pduit
 	return 3*nbPers
 
-def set_tspt_emissions(mode, dist, nbPers):
+def set_tspt_emissions(mode, dist, nbPers, **kw):
 	return mode[0]*dist*nbPers
 
 
-def set_infra_emissions(is_inside, n_hours):
+def set_infra_emissions(is_inside, n_hours, **kw):
 	if(is_inside):
 		return n_hours*3         # 3 correspond aux émissions de CO2 par heure dues au chauffage en intérieur
 	else:
 		return 0
 
-def set_other_emissions():
+def set_other_emissions(**kw):
 	return 0
 	
 
-listFunc = [set_elec_emissions, set_food_emissions, set_tspt_emissions, set_infra_emissions]
+listFunc = [set_elec_emissions, set_food_emissions, set_tspt_emissions, set_infra_emissions, set_other_emissions]
+
 
 def update_emissions(i):
-
-	dicoEmissions[i] = 1
+	dicoEmissions[i] = listFunc[i](argsTotal)
 	return
+
+
 
 
 
