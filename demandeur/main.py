759fce04853d3aad3cd75fa5cc3682c383f74cfe
Inès Bichon <ines.bichon@polytechnique.edu>
2025-02-15 18:00:53 +0100

    test

--- demandeur/main.py
+++ demandeur/main.py
@@ -1,2 +1,21 @@
 import os, json
 from mistralai import Mistral
+
+
+
+tabEmissions = [(),(),(),(),()]
+argsTotal = []
+
+
+def set_tspt_emissions():
+    return mode[0]*dist*nbPers
+
+
+
+
+# def to_text(category, tabArgs, value):
+#     text = ""
+#     if(category==0):
+        
+
+#     return text
